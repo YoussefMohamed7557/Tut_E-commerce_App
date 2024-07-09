611d8307b2a7913b80c41ad6b015d6710e47f2bc
Youssef Mohamed <79748625+YoussefMohamed7557@users.noreply.github.com>
2024-07-09 15:44:46 +0300

    add timer for splash screen and assets for onBoarding screen

--- lib/presentation_layer/splash_view/splash_view.dart
+++ lib/presentation_layer/splash_view/splash_view.dart
@@ -1,7 +1,11 @@
 
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:tut_app/presentation_layer/resources/color_manager.dart';
 import 'package:tut_app/presentation_layer/resources/image_assets.dart';
+import 'package:tut_app/presentation_layer/resources/routes_manager.dart';
+import 'package:tut_app/presentation_layer/resources/constants_manager.dart';
 class SplashView extends StatefulWidget {
   const SplashView({super.key});
 
@@ -10,11 +14,32 @@ class SplashView extends StatefulWidget {
 }
 
 class _SplashViewState extends State<SplashView> {
+  Timer? _timer;
+ _startDelay(){
+   _timer = Timer(const Duration(seconds: ConstantsManager.splashDelay),_goNext);
+ }
+ _goNext(){
+   Navigator.pushReplacementNamed(context, Routes.onBoardingRoute);
+ }
+ @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _startDelay();
+  }
+
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       backgroundColor: ColorManager.PRIMARY,
       body: Center(child:Image.asset(ImageAssets.splashLogo),),
     );
+  }
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _timer?.cancel();
+    super.dispose();
   }
 }
